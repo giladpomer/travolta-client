c7368685838a68dbff254e76e921a87306e2cd74
Gilad Pomer <giladpomer@gmail.com>
2022-12-13 15:36:50 +0100

    consuming search endpoint, setting response data into state

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css';
 //types
 import { Destination } from './types/destination';
 import { SearchParameters } from './types/search-parameters';
+import { HotelSearchResult } from './types/hotel-search-result';
 
 //components
 import MainTitle from './components/MainTitle';
@@ -16,17 +17,20 @@ import { getServerUrl } from './modules/server-url';
 
 function App() {
     const [_destinations, setDestinations] = React.useState<Destination[]>([]);
+    const [_searchResults, setSearchResults] = React.useState<HotelSearchResult[]>([]);
 
     React.useEffect(() => {
         axios.get(getServerUrl() + '/destinations')
             .then(response => {
                 setDestinations(response.data);
-                console.log(response.data);
             });
     }, []);
 
     function onSearchClicked(searchParameters: SearchParameters) {
-        console.log(searchParameters);
+        axios.post(getServerUrl() + '/search', searchParameters)
+            .then(response => {
+                setSearchResults(response.data);
+            });
     }
 
     return (
